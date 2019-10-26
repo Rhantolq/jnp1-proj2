2ab89559bf1c061d31858dec2f733bf0bb99e922
kamilkox <57043007+kamilkox@users.noreply.github.com>
2019-10-26 19:44:15 +0200

    insert function added

--- poset.cc
+++ poset.cc
@@ -1,6 +1,6 @@
-//
-// Created by rhantolq on 26.10.2019.
-//
+	//
+	// Created by rhantolq on 26.10.2019.
+	//
 
 #include "poset.h"
 #include <iostream>
@@ -12,91 +12,91 @@ namespace jnp1 {
     using namespace std;
     using Relations = unordered_set<string*>;
     using Poset = unordered_map<string, pair<Relations, Relations>>;
-
     unsigned long id_counter = 0;
-
     unordered_map<unsigned long, Poset> posets;
-
-
-
-    unsigned long poset_new(void) {
+    
+	unsigned long poset_new(void) {
         id_counter++;
         Poset poset;
         posets[id_counter] = poset;
         return id_counter;
     }
-
-
-    size_t poset_size(unsigned long id) {
-        unordered_map<unsigned long, unordered_map<string, pair<unordered_set<string>, unordered_set<string>>>>::iterator found_poset;
-        found_poset = posets.find(id);
+    
+	size_t poset_size(unsigned long id) {
+        unordered_map<unsigned long, Poset>::iterator found_poset = posets.find(id);
 
         if (found_poset == posets.end()) {
             return 0;
         }
         else {
-            unordered_map<string, pair<unordered_set<string>, unordered_set<string>>> elements = found_poset->second;
+            Poset elements = found_poset->second;
             return elements.size();
-        }
-    }
+	        }
+	}
 
 
-    void poset_delete(unsigned long id) {
+	void poset_delete(unsigned long id) {
+	    if (posets.find(id) != posets.end()) {
+	        posets.erase(id);
+	    }
+	}
 
-        if (posets.find(id) != posets.end()) {
-            posets.erase(id);
-            cout<<"aaa";
-        }
-    }
+	bool poset_insert(unsigned long id, char const *value) {
+		unordered_map<unsigned long, Poset>::iterator found_poset;
+		found_poset = posets.find(id);
+
+		if (found_poset == posets.end()) {
+
+			return false;
+		}
+		else {
+
+			unordered_map<string, pair<unordered_set<string*>, unordered_set<string*>>> *elements;
+			elements = &(found_poset->second);
+			string element(value);
+
+			if ((*elements).find(element) == (*elements).end()) {
+				pair<unordered_set<string*>, unordered_set<string*>> relations;
+				(*elements).insert(pair<string, pair<Relations, Relations>>(element, relations));
+				
+				return true;
+			}
+			else {
+			
+				return false;
+			}
+		}
+
+	}
 
 
-    bool poset_insert(unsigned long id, char const *value) {
-        unordered_map<unsigned long, unordered_map<string, pair<unordered_set<string>, unordered_set<string>>>>::iterator found_poset;
-        found_poset = posets.find(id);
-
-        if (found_poset == posets.end()) {
-
-            return false;
-        }
-        else {
-
-            unordered_map<string, pair<unordered_set<string>, unordered_set<string>>> elements;
-            elements = found_poset->second;
-            string element(value);
-            if (elements.find(element) == elements.end()) {
-                pair<unordered_set<string>, unordered_set<string>> relations;
-                elements.insert(pair<string, pair<unordered_set<string>, unordered_set<string>>>(element, relations));
-
-                return true;
-            }
-            else {
-
-                return false;
-            }
-        }
-
-    }
-
+/*
     bool poset_remove(unsigned long id, char const *value) {
         if (posets.find(id) == posets.end()) {
             return false;
         }
         Poset poset = posets[id];
         
-    }
+    }*/
+}
 
-    int main() {
+using namespace std;
 
-        unordered_set<string> relations;
-        unordered_map<string, unordered_set<string>> elements;
-        unsigned long id =  poset_new();
-        cout << (poset_size(id));
+int main() {
+
+	unordered_set<string> relations;
+	unordered_map<string, unordered_set<string>> elements;
+	unordered_map<string, int> mapa;
+//	mapa["asd"]= 1;
+//	string str2 = &(mapa.find("asd")->first);
+//	cout<<str2<<endl;
 
 
-        poset_delete(id);
-
-
-
-        return 0;
-    }
+	unsigned long id =  jnp1::poset_new();
+	cout << (jnp1::poset_size(id))<<endl;
+	cout << jnp1::poset_insert(id, "abc") << endl;
+	cout<<jnp1::poset_insert(0, "aabc") << endl;
+	cout << (jnp1::poset_size(id)) << endl;
+	jnp1::poset_delete(id);
+	return 0;
 }
